ad241d8489d6a34006d38c7443e65b4bd682c5c6
Yana Kim <Kim_752@mail.ru>
2025-10-08 15:06:31 +0300

    Commit 2

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import s from './App.module.css'
-import HW1 from '../s2-homeworks/hw01/HW1'
+import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import s from './App.module.css';
+import HW1 from '../s2-homeworks/hw01/HW1';
+// import HW2 from '../s2-homeworks/hw02/HW2';
+// import HW3 from '../s2-homeworks/hw03/HW3';
+// import HW4 from '../s2-homeworks/hw04/HW4';
+// import HW5 from '../s2-homeworks/hw05/HW5';
 
 function App() {
     return (
         <div className={s.App}>
-            <HW1/>
-            {/*раскомментировать по ходу выполнения*/}
-            {/*<HW2/>*/}
-            {/*<HW3/>*/}
-            {/*<HW4/>*/}
-
-            {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
-            {/*<HW5/>*/}
+            <HashRouter>
+                <Routes>
+                    <Route path="/" element={<HW1 />} />
+                    {/* Раскомментируй по мере добавления домашних */}
+                    {/* <Route path="/hw2" element={<HW2 />} /> */}
+                    {/* <Route path="/hw3" element={<HW3 />} /> */}
+                    {/* <Route path="/hw4" element={<HW4 />} /> */}
+                    {/* <Route path="/hw5" element={<HW5 />} /> */}
+                </Routes>
+            </HashRouter>
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
