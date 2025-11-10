a49efb24b43539075c9ab130f0d4736dae127f6a
Yana Kim <Kim_752@mail.ru>
2025-11-10 03:45:23 +0300

    Commit 5

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -14,7 +14,6 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
         + (open ? ' ' + s.open : '')
     return (
         <>
-            {/*затемнение справа от открытого меню*/}
             {open && <div className={s.background} onClick={handleClose}/>}
 
             <aside className={sidebarClass}>
@@ -31,17 +30,15 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        className={({ isActive }) => isActive ? s.active : ''}
-                        // className={...} // делает студент
-                        >
+                        className={({isActive}) => isActive ? s.active : ''}
+                    >
                         Pre-junior
                     </NavLink>
                     <NavLink
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        className={({ isActive }) => isActive ? s.active : ''}
-                        // className={...} // делает студент
+                        className={({isActive}) => isActive ? s.active : ''}
                     >
                         Junior
                     </NavLink>
@@ -49,7 +46,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        className={({ isActive }) => isActive ? s.active : ''}
+                        className={({isActive}) => isActive ? s.active : ''}
                     >
                         Junior Plus
                     </NavLink>
